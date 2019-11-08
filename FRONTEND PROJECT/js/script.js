7ac171ff06d19ee238ac3efb77cf9181d53a46f7
Ankesh014 <49806341+Ankesh014@users.noreply.github.com>
2019-11-08 13:23:29 +0530

    Add files via upload
    frontend project based on online shopping

---
+++ FRONTEND PROJECT/js/script.js
@@ -0,0 +1,32 @@
+function validate(){
+    var a = document.getElementById("uname1");
+    if(a.value=="" || b.value=="" ){
+        alert("This Filed cannot be empty");
+    }
+    else{
+    }
+}
+
+function validate2(){
+     var c = document.getElementById("uname");
+    if(c.value==""){
+        alert("Username Required");
+    }
+    else{
+        
+    }
+    var d = document.getElementById("pass");
+    if(d.value==""){
+        alert("Password Required");
+    }
+    else{
+        
+    }
+    var e = document.getElementById("email");
+    if(e.value==""){
+        alert("Email Required");
+    }
+    else{
+        
+    }
+}
